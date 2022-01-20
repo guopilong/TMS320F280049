14e68f856706e2916255fb41e14aed117c9445b1
guopilong <96225311+guopilong@users.noreply.github.com>
2022-01-20 19:14:03 +0800

    Create System_control.c

---
+++ System_control.c
@@ -0,0 +1,82 @@
+
+
+//使能外设时钟
+void InitPeripheralClocks()
+{
+    EALLOW;
+
+    CpuSysRegs.PCLKCR0.bit.CLA1 = 1;
+    CpuSysRegs.PCLKCR0.bit.DMA = 1;
+    CpuSysRegs.PCLKCR0.bit.CPUTIMER0 = 1;
+    CpuSysRegs.PCLKCR0.bit.CPUTIMER1 = 1;
+    CpuSysRegs.PCLKCR0.bit.CPUTIMER2 = 1;
+    CpuSysRegs.PCLKCR0.bit.HRPWM = 1;
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 1;
+
+    CpuSysRegs.PCLKCR2.bit.EPWM1 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM2 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM3 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM4 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM5 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM6 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM7 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM8 = 1;
+
+    CpuSysRegs.PCLKCR3.bit.ECAP1 = 1;
+    CpuSysRegs.PCLKCR3.bit.ECAP2 = 1;
+    CpuSysRegs.PCLKCR3.bit.ECAP3 = 1;
+    CpuSysRegs.PCLKCR3.bit.ECAP4 = 1;
+    CpuSysRegs.PCLKCR3.bit.ECAP5 = 1;
+    CpuSysRegs.PCLKCR3.bit.ECAP6 = 1;
+    CpuSysRegs.PCLKCR3.bit.ECAP7 = 1;
+
+    CpuSysRegs.PCLKCR4.bit.EQEP1 = 1;
+    CpuSysRegs.PCLKCR4.bit.EQEP2 = 1;
+
+    CpuSysRegs.PCLKCR6.bit.SD1 = 1;
+
+    CpuSysRegs.PCLKCR7.bit.SCI_A = 1;
+    CpuSysRegs.PCLKCR7.bit.SCI_B = 1;
+
+    CpuSysRegs.PCLKCR8.bit.SPI_A = 1;
+    CpuSysRegs.PCLKCR8.bit.SPI_B = 1;
+
+    CpuSysRegs.PCLKCR9.bit.I2C_A = 1;
+
+    CpuSysRegs.PCLKCR10.bit.CAN_A = 1;
+    CpuSysRegs.PCLKCR10.bit.CAN_B = 1;
+
+    CpuSysRegs.PCLKCR13.bit.ADC_A = 1;
+    CpuSysRegs.PCLKCR13.bit.ADC_B = 1;
+    CpuSysRegs.PCLKCR13.bit.ADC_C = 1;
+
+    CpuSysRegs.PCLKCR14.bit.CMPSS1 = 1;
+    CpuSysRegs.PCLKCR14.bit.CMPSS2 = 1;
+    CpuSysRegs.PCLKCR14.bit.CMPSS3 = 1;
+    CpuSysRegs.PCLKCR14.bit.CMPSS4 = 1;
+    CpuSysRegs.PCLKCR14.bit.CMPSS5 = 1;
+    CpuSysRegs.PCLKCR14.bit.CMPSS6 = 1;
+    CpuSysRegs.PCLKCR14.bit.CMPSS7 = 1;
+
+    CpuSysRegs.PCLKCR15.bit.PGA1 = 1;
+    CpuSysRegs.PCLKCR15.bit.PGA2 = 1;
+    CpuSysRegs.PCLKCR15.bit.PGA3 = 1;
+    CpuSysRegs.PCLKCR15.bit.PGA4 = 1;
+    CpuSysRegs.PCLKCR15.bit.PGA5 = 1;
+    CpuSysRegs.PCLKCR15.bit.PGA6 = 1;
+    CpuSysRegs.PCLKCR15.bit.PGA7 = 1;
+
+    CpuSysRegs.PCLKCR18.bit.FSIRX_A = 1;
+    CpuSysRegs.PCLKCR18.bit.FSITX_A = 1;
+
+    CpuSysRegs.PCLKCR16.bit.DAC_A = 1;
+    CpuSysRegs.PCLKCR16.bit.DAC_B = 1;
+
+    CpuSysRegs.PCLKCR19.bit.LIN_A = 1;
+
+    CpuSysRegs.PCLKCR20.bit.PMBUS_A = 1;
+
+    CpuSysRegs.PCLKCR21.bit.DCC_0 = 1;
+
+    EDIS;
+}
